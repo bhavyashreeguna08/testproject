6d1c683fd8dd2adbfc1e79b2f29eb752715f8458
bhavyashreeguna08 <bhavyashreeguna08@gmail.com>
2024-11-21 15:34:03 +0530

    pushing the project to repo

---
+++ src/main/java/com/example/TestDemo.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+
+		System.out.println("India is my country.");
+        System.out.println("All Indians are my brothers and sisters.");
+
+	}
+
+}
